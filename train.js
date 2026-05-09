fabf06ce510332365ab8f99611788b614f6ba7e9
Muzaffar <itsmuuza@icloud.com>
2026-05-09 18:10:28 +0900

    task f is completed

--- train.js
+++ train.js
@@ -1,6 +1,23 @@
+// TASK - F
+function findDoublers(data) {
+    let start_index = 0
+    for (let i in data) {
+        for (let j in data) {
+            if (i === j) continue
+            if (data[i] === data[j]) return true
+        }
+    }
+    return false
+}
+
+console.log(findDoublers("hello"));
+
+
 // TASK - E
+/*
 function getReverse(data) {
     return data.split("").reverse().join("");
 }
 
 console.log(getReverse("muzaffar"));
+*/
